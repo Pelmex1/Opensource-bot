334c9b2c85aec6e7b51ee6383aae075725bd9335
Pelmex1 <vkrasovskuy@gmail.com>
2022-11-25 19:33:35 +0200

    fix

--- start.js
+++ start.js
@@ -13,10 +13,10 @@ const config = require('.config/config.json'),
                 }
             }
     });
-module.exports(bot:botInfo);
+module.exports = {bot: botInfo};
 /*if (process.argv.includes('--refresh-slash')) { 
     require('.//.js');
-}/* //На будущее
+} */ //На будущее
 interactionTypes = [
     'ping', 'command',
     'component', 'autocomplete',
@@ -26,34 +26,8 @@ selectMenus = {};
 processTime = Date.now();
 bot.on('ready', async ()=>{
     await bot.user.setPresence({ activities: [{ name: JSON.parse(config.readFileSync('./config.json')).status, type: 5 }]});
-    runtime = Date.now - ${processTime};
+    runtime = Date.now - processTime;
     console.log('bot.user.name запустился за ${runtime}ms');
-path = './'
-    try {
-        if (inter.isCommand()) { path += 'commands/'; }
-        if (inter.isAutocomplete()) { path += 'autocomplete/'; }
-        path += inter.commandName + '/'
-        try {
-            sub = inter.options.getSubcommand()
-            group = inter.options.getSubcommandGroup()
-        }
-        catch(a) { sub = null; group = null }
-        if (inter.options && sub) {
-            if (group) {
-                path += group + '/'
-            }
-            path += sub
-        }
-        path = (path
-            .replaceAll('-', '_')
-            .replaceAll(' ', '_')
-            .toLowerCase() + '.js')
-            .replaceAll('/.js', '.js')
-        console.log(path)
-        path = require(path)
-        path(inter, getLocalisation(data.language))
-                .catch((err) => handleError(inter, err, getLocalisation(data.language)));
-    } catch(err) { handleError(inter, err, getLocalisation(data.language)); }
 });
 bot.on("guildCreate", refreshGuild)
 
